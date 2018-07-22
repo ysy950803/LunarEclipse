d27d38219b206bb06f2d70f3b810b50f75b22412
Shengyu Yao <admin@ysy950803.cn>
2018-07-22 18:01:39 +0800

    Initial commit

---
+++ demo/src/main/java/com/ysy/lunareclipse/demo/EclipseView.java
@@ -0,0 +1,12 @@
+package com.ysy.lunareclipse.demo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class EclipseView extends View {
+
+    public EclipseView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
